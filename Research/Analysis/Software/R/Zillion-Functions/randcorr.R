3b91c71e762c8198ceb964543e6b6352c5a06247
Alex Strobel <87649655+alex-strobel@users.noreply.github.com>
2025-06-04 09:11:59 +0200

    Update randcorr.R

--- Research/Analysis/Software/R/Zillion-Functions/randcorr.R
+++ Research/Analysis/Software/R/Zillion-Functions/randcorr.R
@@ -1,69 +1,52 @@
-randcorr <- function(rho,nobs,nvars,precision=0,matrix.found=F) {
+randcorr <- function(rho, nobs, nvars, precision = 0, matrix.found = FALSE) {
   # ===========================================================================
-  #
   # Purpose: Simulate random correlated variables 
   # Author:  Alexander Strobel (alexander.strobel@tu-dresden.de)
   # Date:    16 Dec 2021
   # Licence: CC0
-  # Input:   rho = a correlation coefficient or matrix, nobs = sample size, 
-  #          nvars = number of correlated variables, precision (see details)  
-  # Output:  a data.frame containing the correlated variables
-  #
   # ===========================================================================
-  #
-  # Details:
-  #
-  # This function returns a matrix of N rows (equaling the number of nobs) by P colums
-  # (equaling the number of nvars) that fulfil the requirement to be correlated as
-  # specified by the numeric value or matrix rho. If rho is a numeric value, all
-  # variables will correlate with r = rho. If rho is a symmetric correlation matrix
-  # (with 1 in the diagonal), the intercorrelations of the variables generated will
-  # reproduce rho to a precison of 0 (default) as a rough approximation using only one
-  # generation cycle or 1 (approxiation to .x), 2 (.xx), and so on (with increasing, but
-  # generally negligible time to find a respective solution). The number of cycles needed
-  # to find a matrix fulfilling the requirements is returned via matrix.found=T.
-  
-  compare <- function(x,y,prec=2) {
-    X=round(x,prec)
-    Y=round(y,prec)
-    return(X==Y)
+  # Arguments:
+  #   rho            Correlation coefficient or correlation matrix
+  #   nobs           Sample size (number of rows)
+  #   nvars          Number of correlated variables (columns)
+  #   precision      Number of decimal places for matrix comparison
+  #   matrix.found   If TRUE, print number of iterations until matching matrix is found
+  # Returns:
+  #   A data.frame with simulated correlated variables
+  # ===========================================================================
+
+  compare_matrices <- function(x, y, prec = 2) {
+    identical(round(x, prec), round(y, prec))
   }
-  
-  if (dim(as.data.frame(rho))[1]==1) {
-    C=matrix(rho,nvars,nvars);
-    for (i in 1:nvars) {
-      C[i,i]=1
-    }
+
+  # Construct correlation matrix if scalar rho is given
+  if (is.numeric(rho) && length(rho) == 1) {
+    C <- matrix(rho, nvars, nvars)
+    diag(C) <- 1
   } else {
-    C=rho
+    C <- rho
   }
 
-  A=chol(C)
+  A <- chol(C)
+  i <- 1
 
-  i=1;
-
-  if (precision==0) {
-    W=rnorm(nobs*nvars,0,1)
-    dim(W)=c(nobs,nvars)
-    y=W%*%A
+  if (precision == 0) {
+    W <- matrix(rnorm(nobs * nvars), nobs, nvars)
+    y <- W %*% A
   } else {
-    k=1;
-    while (k==1) {
-      W=rnorm(nobs*nvars,0,1)
-      dim(W)=c(nobs,nvars)
-      y=W%*%A
-      D=cor(y);
-      c=compare(round(C,precision),round(D,precision),prec=precision)
-      if (all(c)==T) {
-        k=0;
-      }
-      i=i+1
+    repeat {
+      W <- matrix(rnorm(nobs * nvars), nobs, nvars)
+      y <- W %*% A
+      D <- cor(y)
+      matches <- compare_matrices(C, D, prec = precision)
+      if (isTRUE(matches)) break
+      i <- i + 1
     }
-}
-  if (matrix.found==T) {
-    print(paste("matrix found in ",i," steps.",sep=""))
   }
-   
-  return(data.frame(y))
 
+  if (isTRUE(matrix.found)) {
+    message(sprintf("Matrix found in %d steps.", i))
+  }
+
+  return(as.data.frame(y))
 }
