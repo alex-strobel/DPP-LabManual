fe5870f6c76f9f79518876734832022dc61e265c
Alex Strobel <87649655+alex-strobel@users.noreply.github.com>
2025-06-04 08:58:20 +0200

    Update normalize.R

--- Research/Analysis/Software/R/Zillion-Functions/normalize.R
+++ Research/Analysis/Software/R/Zillion-Functions/normalize.R
@@ -1,19 +1,28 @@
 normalize <- function(data, whichFormula = 3, whichMeanSD = 1) {
-  # normalizes data using one of the formulae:
+  # ===========================================================================
   #
-  # 1 = Van der Waerden (1952): r * / (n + 1)
-  # 2 = Blom (1954):           (r - 3/8) / (n + 1/4) (default)
-  # 3 = Bliss (1956; Rankit):  (r - 1/2) / n
-  # 4 = Tukey (1962):          (r - 1/3) / (n + 1/3)
-  #
+  # Purpose: normalizes data using one of the formulae:
+  #          1 = Van der Waerden (1952): r * / (n + 1)
+  #          2 = Blom (1954):           (r - 3/8) / (n + 1/4) (default)
+  #          3 = Bliss (1956; Rankit):  (r - 1/2) / n
+  #          4 = Tukey (1962):          (r - 1/3) / (n + 1/3)
   # Ties will be treated as average, and the output will be rescaled
-  # to have mean = 0 and sd = 1, if you enter 1 for whichMeanSD (default);
-  # if you want to keep the mean and sd of the original variable, enter 0.
+  # to have mean = 0 and sd = 1, if whichMeanSD = 1 (default);
+  # if you want to keep the mean and sd of the original variable, enter 
+  # whichMeanSD = 0.
   #
   # The default normalization algorithm of this function is rankit, as it 
   # has been shown to be superior to other algorithms:
   # 
-  # https://digitalcommons.wayne.edu/jmasm/vol8/iss2/9/
+  # https://digitalcommons.wayne.edu/jmasm/vol8/iss2/9/ 
+  #
+  # Author:  Alexander Strobel (alexander.strobel@tu-dresden.de)
+  # Date:    16 Dec 2021
+  # Licence: CC0
+  # Input:   data = a numeric vector
+  # Output:  normal = the normalized score of the input vector  
+  #
+  # ===========================================================================
   
   normalize = function(data, normalize.formula = whichFormula, mean0.sd1 = whichMeanSD) {
     r = rank(data, na.last = "keep", ties.method = ("average"))
